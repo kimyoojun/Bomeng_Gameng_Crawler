72d4a959a36eb029c5ab23097453bd6244a1b1ae
kimyoojun <anano3134@gmail.com>
2026-07-04 19:28:30 +0900

    docs: 사이트 구분을 위한 도메인 정리

---
+++ schemas/domain.py
@@ -0,0 +1,10 @@
+# 데이터를 수집할 사이트를 도메인 기준으로 분류
+
+# 공식 사이트
+official = ["go.kr"," kr.or", "tour.*.go.kr", "visit*"]
+
+# 블로그
+blog = ["blog.naver.com", "tistory.com", "wordpress.com", "medium.com"]
+
+# 여행 사이트
+trip = ["trip.com", "tripadvisor.com", "agoda.com", "booking.com", "yanolja.com"]
