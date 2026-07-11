ed2474bccf7f6d600c2363da56c2575578d82348
kimyoojun <anano3134@gmail.com>
2026-07-11 14:37:07 +0900

    fix: 크롤링 함수 예외 처리 추가
    크롤링후 text를 반환할때 text 객체가 없는 경우 프로그램이 멈추는 버그 발생
    text객체가 없으면 관광지명을 반환하고 작업이 진행되도록 수정

--- crawling.py
+++ crawling.py
@@ -5,13 +5,14 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 # 블로그 크롤링
-def blog_crawling(url: str):
+def blog_crawling(url: str, spot: str):
     """블로그 내용을 크롤링하는 함수
 
     블로그 링크를 입력하면 블로그 내용(블로그 전체 text)을 크롤링하여 반환함
 
     Args:
         url (str): 블로그 링크
+        spot (str): 관광지 명
 
     Returns:
         str: 블로그 내용(블로그 전체 text)
@@ -23,43 +24,51 @@ def blog_crawling(url: str):
         "User-Agent": "Mozilla/5.0"
     }
 
-    # 블로그 URL에 GET 요청
-    res = requests.get(url, headers=headers)
+    try:
+        # 블로그 URL에 GET 요청
+        res = requests.get(url, headers=headers)
 
-    # 응답 HTML을 BeautifulSoup으로 파싱
-    # "html.parser"는 파이썬 내장 HTML 파서 사용
-    soup = BeautifulSoup(res.text, "html.parser")
-
-    # 현재 페이지 안에서 id가 mainFrame인 iframe을 찾음
-    # 네이버 블로그는 바깥 페이지에 헤더/스킨을 두고
-    # 실제 글 본문을 iframe(mainFrame) 안쪽 별도 페이지에 두는 경우가 많음
-    iframe = soup.select_one("iframe#mainFrame")
-
-    # mainFrame iframe이 존재한다면 -> 본문 페이지로 이동
-    if iframe:
-        # iframe의 src 속성이 실제 본문 url 상대 경로
-        # urljoin을 사용하여 상대 경로를 절대 URL로 변경
-        real_url = urljoin("https://blog.naver.com", iframe["src"])
-
-        # 본문 페이지 URL로 다시 get 요청
-        res = requests.get(real_url, headers=headers)
-
-        # 새로 받은 본문 HTML을 다시 BeautifulSoup으로 파싱
+        # 응답 HTML을 BeautifulSoup으로 파싱
+        # "html.parser"는 파이썬 내장 HTML 파서 사용
         soup = BeautifulSoup(res.text, "html.parser")
 
-    # 본문 전체를 감싸는 선택자 찾기
-    container = soup.select_one("div.se-main-container").text.replace("\n", "")
+        # 현재 페이지 안에서 id가 mainFrame인 iframe을 찾음
+        # 네이버 블로그는 바깥 페이지에 헤더/스킨을 두고
+        # 실제 글 본문을 iframe(mainFrame) 안쪽 별도 페이지에 두는 경우가 많음
+        iframe = soup.select_one("iframe#mainFrame")
 
-    return container
+        # mainFrame iframe이 존재한다면 -> 본문 페이지로 이동
+        if iframe:
+            # iframe의 src 속성이 실제 본문 url 상대 경로
+            # urljoin을 사용하여 상대 경로를 절대 URL로 변경
+            real_url = urljoin("https://blog.naver.com", iframe["src"])
+
+            # 본문 페이지 URL로 다시 get 요청
+            res = requests.get(real_url, headers=headers)
+
+            # 새로 받은 본문 HTML을 다시 BeautifulSoup으로 파싱
+            soup = BeautifulSoup(res.text, "html.parser")
+
+        # 본문 전체를 감싸는 선택자 찾기
+        container = soup.select_one("div.se-main-container").text.replace("\n", "")
+
+    # 파이썬에서 객체(Object)에 존재하지 않는 속성(Attribute)이나 메서드(함수)를 호출하려고 할 때 발생하는 오류
+    except AttributeError:
+        print("오류 발생", spot)
+
+    # 오류가 발생하지 않았을때 실행
+    else:
+        return container
 
 
-def official_crawling(url: str):
+def official_crawling(url: str, spot: str):
     """공식 사이트를 크롤링하는 함수
 
     공식 사이트 링크를 매개변수로 입력하면 사이트 전체 데이터를 반환함
 
     Args:
         url (str): 공식 사이트 링크
+        spot (str): 관광지명
 
     return:
         str: 공식 사이트 내용
@@ -71,15 +80,22 @@ def official_crawling(url: str):
         "User-Agent": "Mozilla/5.0"
     }
 
-    # 블로그 URL에 GET 요청
-    res = requests.get(url, headers=headers)
+    try:
+        # 블로그 URL에 GET 요청
+        res = requests.get(url, headers=headers)
 
-    soup = BeautifulSoup(res.text, "html.parser")
+        soup = BeautifulSoup(res.text, "html.parser")
 
-    name = soup.select_one("h4").text.replace("\n", "")
+        name = soup.select_one("h4").text.replace("\n", "")
 
-    address = soup.select_one(".address").text.replace("\n", "")
+        address = soup.select_one(".address").text.replace("\n", "")
 
-    container = soup.select_one(".grid-container").text.replace("\n", "")
-
-    return name, address, container
+        container = soup.select_one(".grid-container").text.replace("\n", "")
+    
+    # 파이썬에서 객체(Object)에 존재하지 않는 속성(Attribute)이나 메서드(함수)를 호출하려고 할 때 발생하는 오류
+    except AttributeError:
+        print("오류 발생", spot)
+    
+    # 오류가 발생하지 않았을떄 실행
+    else:
+        return name, address, container
